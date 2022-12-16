5227e99547c905b87448637d9bfcc9217cea62e6
Nithin Xavier <nithin.xavier.336@gmail.com>
2022-12-16 16:09:24 +0530

    add dependency injection

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,8 +1,11 @@
 package com.example.demo.Student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudents(){
